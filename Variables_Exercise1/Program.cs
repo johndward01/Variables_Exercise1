4765c3935942b853a3a3bb08f9ae3975d2cfce2d
John Ward <johndward01@gmail.com>
2022-09-12 13:32:43 -0500

    first commit

---
+++ Variables_Exercise1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Declare and initialize variables
+string name = "John";
+int age = 31;
+char middleInitial = 'D';
+bool isOver18 = true;
+double currentTemp = 77.9;
+decimal currentPrice = 29.99m;
+
+// Writing the variables to the console
+Console.WriteLine($"Name: {name}");
+Console.WriteLine($"Age: {age}");
+Console.WriteLine($"Middle Initial: {middleInitial}");
+Console.WriteLine($"Is Over 18: {isOver18}");
+Console.WriteLine($"Current Temp: {currentTemp}");
+Console.WriteLine($"Current Price: {currentPrice}");
